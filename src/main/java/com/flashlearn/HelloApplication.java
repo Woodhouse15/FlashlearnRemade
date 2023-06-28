19317ac41a5f2a7663395f598cdd5335f4259baa
psyew2 <psyew2@nottingham.ac.uk>
2023-06-28 21:19:00 +0100

    connect to mongodb

--- src/main/java/com/flashlearn/HelloApplication.java
+++ src/main/java/com/flashlearn/HelloApplication.java
@@ -4,8 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class HelloApplication extends Application {
     @Override
@@ -19,5 +29,26 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+        char[] password = new char[]{'h','e','l','l','o'};
+        System.out.println(Integer.toHexString(Arrays.hashCode(password)));
+        String connectionString = System.getenv("mongoDBflashlearn");
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                MongoDatabase db = mongoClient.getDatabase("Users");
+                Document user = new Document().append("Eleanor",Integer.toHexString(Arrays.hashCode(password)));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+                db.getCollection("Eleanor").insertOne(user);
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.flashlearn {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires org.mongodb.driver.core;
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.bson;
+
+
     opens com.flashlearn to javafx.fxml;
     exports com.flashlearn;
 }
